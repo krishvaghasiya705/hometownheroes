2e1f3ac587b6482519a72f9d3c36a05f2d5ba64a
krish vaghasiya <146411007+krishvaghasiya705@users.noreply.github.com>
2024-08-27 14:41:07 +0530

    Add files via upload

---
+++ src/common/header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.scss";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="header-container-main"></div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Homepagecomponents/Herobanner/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./herobanner.scss";
+
+function Herobanner() {
+  return (
+    <div>
+      <div>Herobanner</div>
+    </div>
+  );
+}
+
+export default Herobanner;
