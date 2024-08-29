7f09d7adc85042ca285042ae8de591fd3802b6e8
maheshBavishi <hemaxi156.rejoice@gmail.com>
2024-08-29 12:12:43 +0530

    Some issues of design alignment solved

--- src/module/Donate/index.js
+++ src/module/Donate/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Tomvon from "../../components/Lwandlecomponents/Tomvonsection";
 import Lwandleherobanner from "../../components/Lwandlecomponents/Lwandleherobanner";
+import Clocksection from "./../../components/Homepagecomponents/Herobanner/clocksection/index";
 
 function Donate() {
   return (
     <>
       <Lwandleherobanner hideImage={true} showText={true} />
       <Tomvon />
+      <Clocksection
+        borderColor="#F6F1E5"
+        boxBackgroundColor="#F6F1E5"
+        boxFontColor="#1B1B1B"
+        labelFontColor="#F6F1E5"
+        headerFontColor="#F6F1E5"
+      />
     </>
   );
 }
