4bb9b77737084da943472acb0629d4b19801c4d1
maheshBavishi <hemaxi156.rejoice@gmail.com>
2024-08-29 14:51:24 +0530

    clock section added in tomvonbelow page removed extra propertys solved issues of styles

--- src/components/Lwandlecomponents/Tomvonsection/index.js
+++ src/components/Lwandlecomponents/Tomvonsection/index.js
@@ -6,17 +6,11 @@ function Tomvon() {
     <div className="tom-von-section-main">
       <div className="tom-von-section">
         <h4>ARTIST</h4>
-        <div className="scroll-left">
-          {Array(10).fill(<h1>TOM VON BELOW</h1>)}
-        </div>
-        <div className="scroll-right">
-          {Array(10).fill(
-            <h3>
-              A PHOTOGRAPHIC JOURNEY THROUGH THE TOWNSHIPS OF CAPE TOWNMEDIA
-              BRANDING
-            </h3>
-          )}
-        </div>
+        <h1>TOM VON BELOW</h1>
+        <h3>
+          A PHOTOGRAPHIC JOURNEY THROUGH THE TOWNSHIPS OF CAPE TOWNMEDIA
+          BRANDING
+        </h3>
       </div>
     </div>
   );
